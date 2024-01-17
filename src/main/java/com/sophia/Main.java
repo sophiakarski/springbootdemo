28e3d621e202710ccf5580e717a4022e7a556937
sophiakarski <sophiakarski@gmail.com>
2024-01-17 17:27:56 +0000

    Initial commit Spring Boot Demo Project

---
+++ src/main/java/com/sophia/Main.java
@@ -0,0 +1,117 @@
+package com.sophia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@SpringBootApplication
+//@ComponentScan(basePackages = "com.sophia")
+//@EnableAutoConfiguration
+//@Configuration
+@RestController
+@RequestMapping("api/v1/customers")
+public class Main {
+
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping
+    public List<Customer> getCustomers() {
+        return customerRepository.findAll();
+    }
+
+    // GET
+    record NewCustomerRequest(
+            String name,
+            String email,
+            Integer age
+    ) {
+
+    }
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request) {
+        Customer customer = new Customer();
+        customer.setName(request.name());
+        customer.setEmail(request.email());
+        customer.setAge(request.age());
+        customerRepository.save(customer);
+    }
+
+    // DELETE
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer id) {
+        customerRepository.deleteById(id);
+    }
+
+//    @PutMapping("{customerId}")
+//    public void updateCustomer(@PathVariable("customerId") Integer id) {
+//        customerRepository.deleteById(id);
+//    }
+
+    @GetMapping("/greet")
+    public GreetResponse greet() {
+        GreetResponse response = new GreetResponse(
+                "Hello",
+                List.of("Java, Python, Kotlin"),
+                new Person("Alex", 28, 30_000)
+        );
+        return response;
+    }
+
+//    record = class to achieve immutability (constructor, equals, hashcode... all fields are finals)
+    record Person(String name, int age, double savings) {
+
+    }
+    record GreetResponse(
+            String greet,
+            List<String> favProgLangs,
+            Person person
+    ) {
+
+    }
+//    instead of:
+
+//    class GreetResponse {
+//        private final String greet;
+//
+//        public GreetResponse(String greet) {
+//            this.greet = greet;
+//        }
+//
+//        public String getGreet() {
+//            return greet;
+//        }
+//
+//        @Override
+//        public String toString() {
+//            return "GreetResponse{" +
+//                    "greet='" + greet + '\'' +
+//                    '}';
+//        }
+//
+//        @Override
+//        public boolean equals(Object o) {
+//            if (this == o) return true;
+//            if (o == null || getClass() != o.getClass()) return false;
+//            GreetResponse that = (GreetResponse) o;
+//            return Objects.equals(greet, that.greet);
+//        }
+//
+//        @Override
+//        public int hashCode() {
+//            return Objects.hash(greet);
+//        }
+//    }
+
+}
+
+// Tomcat webserver - change app so that when make a response, return response
